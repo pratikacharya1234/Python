95e013c4946e7a1774a65594fa402c8aed01b3ed
pratikacharya1234 <acharyapratik214@gmail.com>
2025-03-25 09:11:21 -0600

    Add initial implementation of sorting functions in DSA/index.py

---
+++ DSA/index.py
@@ -0,0 +1,34 @@
+# arr = ['5', '4', '3', '2', '1']
+
+# def rearange(arr):
+#     for i in range (len(arr)):
+#         for j in range (0, len(arr)-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+#     return arr
+# print(rearange(arr))
+
+
+
+# arr = ['10','23','44','666']
+
+# def rearange(arr):
+#     for i in range(len(arr)):
+#         for j in range (0,len(arr)-i-1):
+#             if arr[j] > arr[j+1]:
+#                 arr[j], arr[j+1] = arr[j+1], arr[j]
+
+#     return arr
+# print(rearange(arr))
+
+
+
+# arr = ['2','4','5','2','3']
+
+# def reverseArrange(arr):
+#     for i in range(len(arr)):
+#         for j in range (0, len(arr)-i-1):
+#             if arr[j] < arr[j+1]:
+#                arr[j], arr[j+1] = arr[j+1], arr[j]
+#     return arr
+# print(reverseArrange(arr))
